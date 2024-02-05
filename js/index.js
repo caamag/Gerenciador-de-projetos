5d3cbc353a07068461f8335f504d8af742760e54
BCR-0632 <caiolopesfv@gmail.com>
2024-02-05 19:26:08 -0300

    enviando atuliações

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
-
 //login
+const inputUser = document.querySelector('#userName')
+const passInput = document.querySelector('#pass')
+const errorLogin = document.querySelector('.login span')
+
+const form = document.querySelector('.login-form')
+
+const container = document.querySelector('.container')
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+    const user = inputUser.value; 
+    const pass = passInput.value;
+
+    if (user === "florence" && pass === "@florence123") {
+        form.style.display = 'none'
+        errorLogin.style.display = 'none'
+        container.style.display = 'block'
+    }else{
+        errorLogin.style.display = 'block'
+    }
+
+})
