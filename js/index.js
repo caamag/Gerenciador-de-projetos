fff5b3790e6bdf0492cc1d3e53aa3506889832a9
Caio Lopes <caiolopesfv@gmail.com>
2024-02-06 19:45:30 -0300

    enviando atualizaçõies

--- js/index.js
+++ js/index.js
@@ -10,15 +10,32 @@ const container = document.querySelector('.container')
 form.addEventListener('submit', (e) => {
 
     e.preventDefault()
-    const user = inputUser.value; 
+    const user = inputUser.value;
     const pass = passInput.value;
 
     if (user === "florence" && pass === "@florence123") {
         form.style.display = 'none'
         errorLogin.style.display = 'none'
         container.style.display = 'block'
-    }else{
+    } else {
         errorLogin.style.display = 'block'
     }
 
 })
+
+
+
+
+//current date
+const date = new Date()
+const year = date.getFullYear()
+const month = date.getMonth() + 1
+const day = date.getDate()
+
+//format
+const dayFormat = day < 10 ? '0' + day : day
+const monthFormat = month < 10 ? '0' + month : month
+const dateText = `${dayFormat}/${monthFormat}/${year}`
+
+const currentDate = document.querySelector('.date')
+currentDate.innerHTML = dateText
